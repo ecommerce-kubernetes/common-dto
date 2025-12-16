d23163077a1eea8969dc9384da024665edd392a6
minsik2434 <minsik2434@gmail.com>
2025-12-16 19:34:01 +0900

    새 데이터모델 생성

--- common-event/src/main/java/com/example/common/SagaProcessResult.java
+++ common-event/src/main/java/com/example/common/SagaProcessResult.java
@@ -2,8 +2,10 @@ package com.example.common;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class SagaProcessResult {
     private Long sagaId;
     private Long orderId;
